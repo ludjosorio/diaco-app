b0725dea540fc4bbe87bbddc7e7abfcd02fd253f
ludjos <ludjosorio@gmail.com>
2021-09-18 16:22:41 -0600

    create complaint, provider, and consumer

--- src/app/component/complain/complain.component.ts
+++ src/app/component/complain/complain.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IGender } from 'src/app/interface/interface';
 import { ApiService } from 'src/app/services/api/api.service';
 import { ToastService } from 'src/app/theme/shared/components/toast/toast.service';
-import { FormArray, FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -12,14 +12,13 @@ import { FormArray, FormBuilder, Validators } from '@angular/forms';
 })
 export class ComplainComponent implements OnInit {
 
-  public diacoBranches: Array<any> = [];
   public nationalities: Array<any> = [];
   public genders: Array<IGender> = [];
   public typeConsumers: Array<any> = [];
   public departments: Array<any> = [];
-  public providerDepartments: Array<any> = [];
+  public complaintDepartments: Array<any> = [];
   public municipalities: Array<any> = [];
-  public providerMunicipalities: Array<any> = [];
+  public complaintMunicipalities: Array<any> = [];
   public numberZones: Array<number> = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
 
   public anonymous: boolean = false;
@@ -28,36 +27,34 @@ export class ComplainComponent implements OnInit {
 
   public complaintForm = this.fb.group({
     consumer: this.fb.group({
-      id: ['', Validators.required],
+      id: [],
       name: ['', Validators.required],
-      email: ['', Validators.required],
-      dpi: ['', Validators.required],
-      homePhone: ['', Validators.required],
-      phone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      dpi: ['', [Validators.required, Validators.pattern(/^[0-9]{8,13}$/)]],
+      homePhone: ['', [Validators.required, Validators.pattern(/^[0-9]{8,8}$/)]],
+      phone: ['', [Validators.required, Validators.pattern(/^[0-9]{8,8}$/)]],
       nationalityId: ['', Validators.required],
       typeConsumerId: ['', Validators.required],
       genderId: ['', Validators.required],
       departmentId: ['', Validators.required],
       municipalityId: ['', Validators.required],
-      address: ['', Validators.required],
+      address: ['', [Validators.required]],
     }),
     provider: this.fb.group({
-      id: ['', Validators.required],
+      id: [],
       name: ['', Validators.required],
       businessName: ['', Validators.required],
-      nit: ['', Validators.required],
-      address: ['', Validators.required],
-      zoneNumber: ['', Validators.required],
-      departmentId: ['', Validators.required],
-      municipalityId: ['', Validators.required],
-      phone: ['', Validators.required],
-      email: ['', Validators.required],
+      nit: ['', [Validators.required, Validators.pattern(/^[0-9]{8,8}$/)]],
+      phone: ['', [Validators.required, Validators.pattern(/^[0-9]{8,8}$/)]],
+      email: ['', [Validators.required, Validators.email]],
     }),
     invoiceNumber: ['', Validators.required],
     invoiceDate: ['', Validators.required],
-    complaintDetail: ['', Validators.required],
-    complaintRequest: ['', Validators.required],
-    branchId: ['', Validators.required],
+    complaintDetail: ['', [Validators.required, Validators.minLength(25), Validators.maxLength(250)]],
+    complaintRequest: ['', [Validators.required, Validators.minLength(25), Validators.maxLength(250)]],
+    address: ['', Validators.required],
+    departmentId: ['', Validators.required],
+    municipalityId: ['', Validators.required],
   });
 
   constructor(
@@ -79,25 +76,25 @@ export class ComplainComponent implements OnInit {
   setAnonymous() {
     this.anonymous = !this.anonymous;
     this.complaintForm.get('consumer').reset();
-
+    this.complaintForm.controls.consumer.enable();
   }
 
   async getDependencyListings(): Promise<void> {
     try {
-      const [typeConsumers, genders, nationalities, departments, diacoBranches] = await Promise.all([
+      this.complaintForm.disable();
+      const [typeConsumers, genders, nationalities, departments] = await Promise.all([
         this.api.get('/consumer/type-consumer'),
         this.api.get('/consumer/gender'),
         this.api.get('/consumer/nacionality'),
         this.api.get('/department'),
-        this.api.get('/department/diaco-branch'),
       ]);
       this.genders = genders.data;
       this.nationalities = nationalities.data;
-      this.diacoBranches = diacoBranches.data;
       this.typeConsumers = typeConsumers.data;
       this.departments = departments.data;
-      this.providerDepartments = departments.data;
+      this.complaintDepartments = departments.data;
       this.loadDependencies = false;
+      this.complaintForm.enable();
     } catch (error) {
       this.toastMessage = error.message || 'Ha ocurrido un error';
       this.toastEvent.toast({ uid: 'toast6', delay: 3000 });
@@ -105,15 +102,37 @@ export class ComplainComponent implements OnInit {
     }
   }
 
-  validateForm() {
-    console.log(this.complaintForm.invalid);
+  async createComplaint() {
+    try {
+      this.findInvalidControls();
+      const data = this.complaintForm.getRawValue();
+      if (this.anonymous) {
+        data.consumer = Object({});
+        data.anonymous = true;
+        this.complaintForm.controls.consumer.disable();
+      } else {
+        data.anonymous = false;
+        this.complaintForm.controls.consumer.enable();
+      }
+      // this.findInvalidControls();
+      await this.api.post('/complain', data);
+    } catch (error) {
+      this.toastMessage = error.message || 'Ha ocurrido un error';
+      this.toastEvent.toast({ uid: 'toast6', delay: 3000 });
+    }
   }
 
-  async createComplaint() {
-    this.validateForm();
-    return;
-    const data = this.complaintForm.getRawValue();
-    await this.api.post('/complain', data);
+  public findInvalidControls() {
+    const invalid = [];
+    const controls = this.complaintForm.controls;
+    for (const name in controls) {
+      if (controls[name].invalid) {
+        invalid.push(name);
+      }
+    }
+
+    console.log('asdfa', invalid);
+    return invalid;
   }
 
   selectConsumerDepartment($event) {
@@ -125,10 +144,10 @@ export class ComplainComponent implements OnInit {
     }
   }
 
-  selectProviderDepartment($event) {
+  selectComplaintDepartament($event) {
     try {
-      const municipalities = this.refreshMunicipalities(this.providerDepartments, this.providerMunicipalities, $event);
-      this.providerMunicipalities = municipalities;
+      const municipalities = this.refreshMunicipalities(this.complaintDepartments, this.complaintMunicipalities, $event);
+      this.complaintMunicipalities = municipalities;
     } catch (error) {
 
     }
@@ -142,10 +161,12 @@ export class ComplainComponent implements OnInit {
 
   async searchConsumer() {
     try {
-      const dpi = this.complaintForm.get('consumer').get('dpi').value;
-      if (dpi) {
+      var formValues = this.complaintForm.get('consumer').value;
+      console.log('formValues', formValues);
+
+      if (formValues.dpi) {
         this.complaintForm.get('consumer').reset();
-        const consumer = await this.api.get('/consumer/' + dpi);
+        const consumer = await this.api.get('/consumer/' + formValues.dpi);
         this.complaintForm.controls.consumer.patchValue(consumer.data);
         const municipality = this.setDepartmentAndMunicipality('consumer', consumer.data.municipalityId);
         this.municipalities = municipality.municipalities;
@@ -153,8 +174,12 @@ export class ComplainComponent implements OnInit {
         const phone = this.complaintForm.get('consumer').get('phone').value;
         this.complaintForm.get('consumer').get('homePhone').setValue('0000' + homePhone)
         this.complaintForm.get('consumer').get('phone').setValue('0000' + phone)
+        this.complaintForm.controls.consumer.disable();
+        this.complaintForm.controls.consumer.get('dpi').enable()
       }
     } catch (error) {
+      this.complaintForm.get('consumer').patchValue(formValues);
+      this.complaintForm.controls.consumer.enable();
       this.toastMessage = error.message;
       this.toastEvent.toast({ uid: 'toast6', delay: 3000 });
       console.log(error);
@@ -163,15 +188,17 @@ export class ComplainComponent implements OnInit {
 
   async searchProvider() {
     try {
-      const nit = this.complaintForm.get('provider').get('nit').value;
+      var nit = this.complaintForm.get('provider').get('nit').value;
       if (nit) {
         this.complaintForm.get('provider').reset();
         const provider = await this.api.get('/provider/' + nit);
         this.complaintForm.controls.provider.patchValue(provider.data);
-        const municipality = this.setDepartmentAndMunicipality('provider', provider.data.municipalityId);
-        this.providerMunicipalities = municipality.municipalities;
+        this.complaintForm.controls.provider.disable();
+        this.complaintForm.controls.provider.get('nit').enable()
       }
     } catch (error) {
+      this.complaintForm.get('provider').get('nit').setValue(nit);
+      this.complaintForm.controls.provider.enable();
       this.toastMessage = error.message;
       this.toastEvent.toast({ uid: 'toast6', delay: 3000 });
       console.log(error);
